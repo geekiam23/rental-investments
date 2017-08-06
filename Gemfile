5b05531c591b60599f662b9ae464e8af4f450e3c
Will Larry <will23larry@gmail.com>
2017-08-06 15:37:33 -0500

    Added Bootstrap and Reconfig Routes

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'bootstrap'
 gem 'figaro'
+gem 'bootstrap-sass'
 
 group :development, :test do
   gem 'byebug'
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
 
-  get 'welcome/about'
-
-  get 'welcome/contact'
+  get 'about' => 'welcome#about'
+  get 'contact' => 'welcome#contact'
 
   root 'welcome#index'
 end
